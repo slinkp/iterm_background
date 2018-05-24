ad129676b1fddcfe786969e092dd7af3d2ce64dc
Paul Winkler <paul.winkler@handshake.com>
2018-05-24 11:49:11 -0400

    Remove debug blather

--- iterm_set_bg_for_host.sh
+++ iterm_set_bg_for_host.sh
@@ -9,16 +9,11 @@ fi
 function generate_text_image {
     # Depends on `convert` from ImageMagick.
     # TODO:
-    echo HELLLOOO >> /tmp/argh
     TEXT="$1"
     IMG_PATH="$2"
     /usr/local/bin/convert -background black -fill white -font "$FONT" -size 400x200 \
             -rotate 20 "label:${TEXT}" "${IMG_PATH}"  >> /tmp/argh 2>&1
-    echo "convert -background black -fill white -font \"${ITERM_BG_FONT}\" -size 400x200 -rotate 20 \"label:${TEXT}\" \"$IMG_PATH\"" >> /tmp/argh
 }
-
-NOW=`date`
-echo "I RAN AT $NOW" >> /tmp/argh
 
 TEXT="$1"
 
@@ -45,13 +40,9 @@ fi
 
 
 if [ -n "$IMG_PATH" ]; then
-    echo Will set image as "$IMG_PATH" >> /tmp/argh
     if [ ! -f "$IMG_PATH" ]; then
-        echo Generating new image at "$IMG_PATH" >> /tmp/argh
         generate_text_image "$TEXT" "$IMG_PATH"
     fi
-else
-    echo Will clear image >> /tmp/argh
 fi
 
 osascript $HERE/iterm_set_bg.scpt "$IMG_PATH"
